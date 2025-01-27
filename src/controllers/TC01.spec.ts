02fe16ed0fb21838a8bf4fdc8e3d940727205b67
Manuel Meneses <manuelrmenesesr@gmail.com>
2025-01-26 20:06:32 -0600

    feat(TC01) data provider implementation

--- src/controllers/TC01.spec.ts
+++ src/controllers/TC01.spec.ts
@@ -4,39 +4,41 @@ import { NavBar } from "../components/navBar/navBar.component.js";
 import { Product } from "../components/product/product.component.js";
 import { SearchResult } from "../components/searchResult/searchResult.component.js";
 import { OrderPayment } from "../components/orderPayment/orderPayment.component.js";
-import authData from "../models/auth.mock.json" assert { type: 'json' };
+import authData from "../models/auth.mock.json" assert { type: "json" };
+
+type DataProducts = {
+  searchData: string;
+  product: string;
+};
+
+type DataProviderElement = {
+  products: DataProducts[];
+};
 
 export class TC01 extends Controller {
-
   constructor() {
     super("Proceso de compra E2E");
   }
 
-  protected async workflow(data: unknown) {
+  protected async workflow(data: DataProviderElement) {
     const home = new Home(this.page);
     const searchResult = new SearchResult(this.page);
     const product = new Product(this.page);
     const navBar = new NavBar(this.page);
     const orderPayment = new OrderPayment(this.page);
 
-    await home.clickOnSearchOption();
-    await home.enterSearchData("Chromebook");
-    await home.clickOnSearch();
-    await searchResult.searchResultAssert();
-    await searchResult.clickOnProduct("HP Chromebook 14 G1(ENERGY STAR)");
-    await product.clickOnAddToCart();
-    await navBar.goHome();
-    await home.clickOnSearchOption();
-    await home.enterSearchData("Headphones");
-    await home.clickOnSearch();
-    await searchResult.searchResultAssert();
-    await searchResult.clickOnProduct(
-      "Beats Studio 2 Over-Ear Matte Black Headphones"
-    );
-    await product.clickOnAddToCart();
+    data.products.forEach(async (dataProduct) => {
+      await navBar.goHome();
+      await home.clickOnSearchOption();
+      await home.enterSearchData(dataProduct.searchData);
+      await home.clickOnSearch();
+      await searchResult.searchResultAssert();
+      await searchResult.clickOnProduct(dataProduct.product);
+      await product.clickOnAddToCart();
+    });
     await navBar.clickOnCheckout();
     await orderPayment.login(authData.username, authData.password);
-    await orderPayment.clickOnNext();
-    await orderPayment.clickOnPayNow();
+    // await orderPayment.clickOnNext();
+    // await orderPayment.clickOnPayNow();
   }
 }
--- src/tests/TC01.test.ts
+++ src/tests/TC01.test.ts
@@ -1,8 +1,11 @@
 import { TC01 } from "../controllers/TC01.spec.js";
 import { test } from "./.hooks.js";
+import data from "../models/TC01.mock.json" assert { type: "json" };
 
 const tc01 = new TC01();
 
-test(tc01.getDescription(), async ({ page }) => {
-  await tc01.exec(page);
+data.forEach((products, index) => {
+  test(`${tc01.getDescription()} - Iteración ${index + 1}`, async ({ page }) => {
+    await tc01.exec(page, products);
+  });
 });
